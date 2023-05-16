cea985a4735e88046c777f8a2cbb69b9b553aa7d
FabioMateus1510 <fabiomateus1510@gmail.com>
2023-05-16 16:45:19 -0300

    campos personalizaveis

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let wheel = document.querySelector('.wheel');
-let spinBtn = document.querySelector('.spinBtn');
+const wheel = document.querySelector('.wheel');
+const spinBtn = document.querySelector('.spinBtn');
 let value = Math.ceil(Math.random() * 3600);
+
+const inputs = document.querySelectorAll('.input');
+const segments = document.querySelectorAll('.number');
+const segmentsValue = document.querySelectorAll('.segment-value');
 
 spinBtn.onclick = function () {
   wheel.style.transform = 'rotate(' + value + 'deg)';
   value += Math.ceil(Math.random() * 3600);
 };
+
+const batata = (position) => {
+  segmentsValue[position].textContent = inputs[position].value;
+};
+
+inputs.forEach((input, index) => {
+  input.addEventListener('keydown', () => batata(index));
+});
